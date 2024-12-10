b51cc615d983bc0751acea00bf6105c3e1a5c1d6
Shadesman <n.karpov90@gmail.com>
2024-12-10 11:19:46 +0100

    dynamic robots.txt

--- index.php
+++ index.php
@@ -1,5 +1,50 @@
 <?php
 
+// Prüfe, ob die Anfrage für die robots.txt-Datei ist
+if (preg_match('/robots\.txt$/', $_SERVER['REQUEST_URI'])) {
+    if ($_SERVER['REQUEST_URI'] !== '/robots.txt') {
+        http_response_code(404);
+        exit('Not Found');
+    }
+
+    // Setze den Header für den Textinhalt
+    header('Content-Type: text/plain');
+    
+    // Hole die aktuelle Domain
+    $domain = $_SERVER['HTTP_HOST'];
+
+    // Dynamische Ausgabe der robots.txt
+    echo "User-agent: *\n";
+    echo "User-agent: AdsBot-Google\n";
+    echo "Disallow: /\n\n";
+
+    echo "### Sitemap\n";
+    echo "Sitemap: https://$domain/sitemap.xml\n\n";
+
+    echo "### Folders\n";
+    echo "Allow: /storage/\n";
+    echo "Disallow: /userstorage/\n";
+    echo "Disallow: /app/\n";
+    echo "Disallow: /packages/\n";
+    echo "Disallow: /admin/\n";
+    echo "Disallow: /node_modules/\n";
+    echo "Disallow: /user/login\n";
+    echo "Disallow: /tmp/\n\n";
+
+    echo "### Files\n";
+    echo "Disallow: /autoload.php\n";
+    echo "Disallow: /config.php\n\n";
+
+    echo "### Filetypes\n";
+    echo "Disallow: *.db\n";
+    echo "Disallow: *.json\n";
+    echo "Disallow: *.lock\n";
+    echo "Disallow: *.md\n";
+
+    // Beende das Skript nach Ausgabe der robots.txt
+    exit();
+}
+
 if (version_compare($ver = PHP_VERSION, $req = '7.2.0', '<')) {
     exit(sprintf('You are running PHP %s, but TTAGS System needs at least <strong>PHP %s</strong> to run.', $ver, $req));
 }
