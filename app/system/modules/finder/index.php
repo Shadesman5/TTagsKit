18a08aa42255629c4a338cfaa8550c7c71b79c28
Shadesman <n.karpov90@gmail.com>
2024-12-10 12:07:08 +0100

    added userstorage routes, path, permissions etc.

--- app/system/modules/finder/index.php
+++ app/system/modules/finder/index.php
@@ -14,6 +14,11 @@ return [
         $this->config['storage'] = '/' . trim(($this->config['storage'] ?: 'storage'), '/');
         $app['path.storage'] = $app['path'] . $this->config['storage'];
         $app['locator']->add('storage:', $app['path.storage']);
+
+        // Define the user storage path
+        $this->config['userstorage'] = '/' . trim(($this->config['userstorage'] ?: 'userstorage'), '/');
+        $app['path.userstorage'] = $app['path'] . $this->config['userstorage'];
+        $app['locator']->add('userstorage:', $app['path.userstorage']);
     },
 
     'routes' => [
@@ -25,6 +30,10 @@ return [
         '/site/storage' => [
             'name' => '@site/storage',
             'controller' => 'Pagekit\\Finder\\Controller\\StorageController'
+        ],
+        '/user/userstorage' => [
+            'name' => '@user/userstorage',
+            'controller' => 'Pagekit\\Finder\\Controller\\UserStorageController'
         ]
 
     ],
@@ -47,7 +56,8 @@ return [
         'view.system:modules/settings/views/settings' => function ($event, $view) use ($app) {
             $view->data('$settings', [
                 'config' => [
-                    $this->name => ['storage' => $this->config['storage'] === '/storage' ? '' : $this->config['storage']]
+                    $this->name => ['storage' => $this->config['storage'] === '/storage' ? '' : $this->config['storage']],
+                    $this->name => ['userstorage' => $this->config['userstorage'] === '/userstorage' ? '' : $this->config['userstorage']]
                 ],
                 'options' => [
                     $this->name => ['extensions' => $this->config['extensions']]
@@ -57,7 +67,24 @@ return [
 
         'system.finder' => function ($event) use ($app) {
             if ($app['user']->hasAccess('system: manage storage | system: manage storage read only')) {
-                $event->path('#^' . preg_quote(strtr($app['path.storage'], '\\', '/'), '#') . '($|\/.*)#', $app['user']->hasAccess('system: manage storage') ? 'w' : 'r');
+                $event->path(
+                    '#^' . preg_quote(strtr($app['path.storage'], '\\', '/'), '#') . '($|\/.*)#',
+                    $app['user']->hasAccess('system: manage storage') ? 'w' : ($app['user']->hasAccess('system: manage storage read only') ? 'r' : false)
+                );
+            }
+
+            if ($app['user']->hasAccess('system: manage all userstorages')) {
+                $pathPattern = '#^' . preg_quote(strtr($app['path.userstorage'], '\\', '/'), '#') . '.*$#';
+                $event->path($pathPattern, 'w');
+            } else {
+                // Standardzugriff für normale Benutzer auf ihr eigenes Verzeichnis
+                $userId = $app['user']->id;
+                $userStoragePath = $app['path.userstorage'] . '/' . $userId;
+                $pathPattern = '#^' . preg_quote(strtr($userStoragePath, '\\', '/'), '#') . '($|\/.*)#';
+                $event->path(
+                    $pathPattern,
+                    $app['user']->hasAccess('system: manage userstorage') ? 'w' : ($app['user']->hasAccess('system: manage userstorage read only') ? 'r' : false)
+                );
             }
         }
 
@@ -66,11 +93,22 @@ return [
     'permissions' => [
 
         'system: manage storage' => [
-            'title' => 'Manage storage',
+            'title' => 'Manage Storage',
             'trusted' => true
         ],
         'system: manage storage read only' => [
-            'title' => 'Manage storage (Read only)'
+            'title' => 'Manage Storage (Read only)'
+        ],
+        'system: manage all userstorages' => [
+            'title' => 'Manage all User Storages',
+            'trusted' => true
+        ],
+        'system: manage userstorage' => [
+            'title' => 'Manage User Storage',
+            'trusted' => true
+        ],
+        'system: manage userstorage read only' => [
+            'title' => 'Manage User Storage (Read only)'
         ]
 
     ],
@@ -89,7 +127,7 @@ return [
             'label' => 'Storage',
             'parent' => 'site',
             'url' => '@site/storage',
-            'access' => 'system: manage storage',
+            'access' => 'system: manage storage | system: manage storage read only',
             'priority' => 20
         ]
 
@@ -98,6 +136,8 @@ return [
     'config' => [
 
         'storage' => false,
+
+        'userstorage' => false,
 
         'extensions' => 'bmp,gif,jpeg,jpg,mp4,ogg,pdf,png,svgz,svg,swf'
 
