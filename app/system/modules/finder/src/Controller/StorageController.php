01b8ce6c0f2dd9a712be9a4dc8f32cc533c40837
Shadesman <n.karpov90@gmail.com>
2024-12-10 12:34:22 +0100

    added variables and permission for manage storage read-only

--- app/system/modules/finder/src/Controller/StorageController.php
+++ app/system/modules/finder/src/Controller/StorageController.php
@@ -5,19 +5,27 @@ namespace Pagekit\Finder\Controller;
 use Pagekit\Application as App;
 
 /**
- * @Access("system: manage storage", admin=true)
+ * @Access("system: manage storage | system: manage storage read only", admin=true)
  */
 class StorageController
 {
     public function indexAction()
     {
+        if (App::user()->hasAccess('system: manage storage')) {
+            $mode = 'w';
+        } elseif (App::user()->hasAccess('system: manage storage read only')) {
+            $mode = 'r';
+        } else {
+            App::abort(403, __('Insufficient User Permissions.'));
+        }
+
         return [
             '$view' => [
                 'title' => __('Storage'),
                 'name'  => 'system:modules/finder/views/storage.php'
             ],
             'root' => App::module('system/finder')->config('storage'),
-            'mode' => 'write'
+            'mode' => $mode
         ];
     }
 }
